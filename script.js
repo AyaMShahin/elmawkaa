29dd5c53c4b54e8ef747f292daf78c08f7a12a40
Aya Shahin <66850776+AyaMShahin@users.noreply.github.com>
2023-02-01 02:06:22 +0200

    header has been edited

---
+++ script.js
@@ -0,0 +1,32 @@
+// Material Input List
+const show = () => {
+  document.querySelector(".materials").classList.add("active");
+};
+
+const hide = () => {
+  document.querySelector(".materials").classList.remove("active");
+};
+// ٍSearch for materials
+function search() {
+  let searchBar = document
+    .querySelector(".search-material")
+    .value.toUpperCase();
+  // let materialItem = document.querySelector(".materials");
+  let materialName = document.getElementsByTagName("a");
+  for (let index = 0; index < materialName.length; index++) {
+    if (materialName[index].innerHTML.toUpperCase().indexOf(searchBar) >= 0) {
+      materialName[index].style.display = "";
+    } else {
+      materialName[index].style.display = "none";
+    }
+  }
+}
+// Edit file input style
+const input = document.getElementById("inputStyle");
+let imageName = document.querySelector(".upload");
+
+input.addEventListener("change", () => {
+  let inputImage = document.querySelector("input[type=file]").files[0];
+
+  imageName.innerText = inputImage.name;
+});
