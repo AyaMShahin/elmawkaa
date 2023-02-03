716c7a090964e7fa5bb931256bdb2cdb9c25ec26
Aya Shahin <66850776+AyaMShahin@users.noreply.github.com>
2023-02-03 05:34:27 +0200

    validation form has been added

--- formScript.js
+++ formScript.js
@@ -1,5 +1,7 @@
 "use strict";
-
+const date = document.getElementById("date");
+const company = document.getElementById("company");
+date.min = new Date().toISOString().split("T")[0];
 // Material Input List
 const show = () => {
   document.querySelector(".materials").classList.add("active");
@@ -24,14 +26,16 @@ function search() {
   }
 }
 
+// DisplayPage(pagination, currentPage);
+// setUpPagination(pagination);
 // Validation of Product Info
+
 function validation() {
   const errorName = document.getElementById("nError"),
     errorQuantity = document.getElementById("qError"),
     errorUnits = document.getElementById("uError"),
     errorRate = document.getElementById("rError"),
     errorType = document.getElementById("tError");
-  const regPhone = /^\d{10}$/; // Javascript reGex for Phone Number validation.
   const regName = /\d+$/g; // Javascript reGex for Name validation
   const name = document.forms.RegForm.name.value,
     quantity = document.forms.RegForm.Quantity.value,
@@ -42,21 +46,21 @@ function validation() {
   // if a form field is empty, this function writes a message, and returns false, to prevent the form from being submitted:
   if (name == "" || regName.test(name)) {
     errorName.innerHTML = "هذه الخانة مطلوبة";
-    console.log("Empty");
+    // console.log("Empty");
     return false;
   } else if (name !== "") {
     errorName.innerHTML = "";
   }
   if (quantity == "") {
     errorQuantity.innerHTML = "هذه الخانة مطلوبة";
-    console.log("Empty");
+    // console.log("Empty");
     return false;
   } else if (quantity !== "") {
     errorQuantity.innerHTML = "";
   }
   if (units.selectedIndex == -1) {
     errorUnits.innerHTML = "هذه الخانة مطلوبة";
-    console.log("Empty");
+    // console.log("Empty");
     return false;
   } else if (units.selectedIndex !== -1) {
     errorUnits.innerHTML = "";
@@ -64,7 +68,7 @@ function validation() {
   }
   if (cementRate.selectedIndex == -1) {
     errorRate.innerHTML = "هذه الخانة مطلوبة";
-    console.log("Empty");
+    // console.log("Empty");
     return false;
   } else if (cementRate.selectedIndex !== -1) {
     errorRate.innerHTML = "";
@@ -72,11 +76,112 @@ function validation() {
   }
   if (cementType.selectedIndex == -1) {
     errorType.innerHTML = "هذه الخانة مطلوبة";
-    console.log("Empty");
+    // console.log("Empty");
     return false;
   } else if (cementType.selectedIndex !== -1) {
     errorType.innerHTML = "";
     // return true;
   }
-  return true;
+  return (check = true);
+}
+function projectValidate() {
+  const errorLocation = document.getElementById("lError"),
+    errorAddress = document.getElementById("adError"),
+    errorLevel = document.getElementById("levError"),
+    errorDate = document.getElementById("dError");
+  // D = document.getElementById("tError");
+
+  const location = document.forms.RegForm.place.value,
+    address = document.forms.RegForm.address.value,
+    levels = document.forms.RegForm.level.value;
+  // date = document.forms.RegForm.rate.value,
+
+  // Create a new Date that converts the input date
+  let dateValue = new Date(date.value);
+  // Extract pieces of the date:
+  let pattern =
+    /^((0[1-9]|[12][0-9]|3[01])(\/)(0[13578]|1[02]))|((0[1-9]|[12][0-9])(\/)(02))|((0[1-9]|[12][0-9]|3[0])(\/)(0[469]|11))(\/)\d{4}$/;
+
+  if (location.selectedIndex == -1) {
+    errorLocation.innerHTML = "هذه الخانة مطلوبة";
+    // console.log("Empty");
+    return false;
+  } else if (location.selectedIndex !== -1) {
+    errorLocation.innerHTML = "";
+    // return true;
+  }
+
+  if (address == "") {
+    errorAddress.innerHTML = "هذه الخانة مطلوبة";
+    // console.log("Empty");
+    return false;
+  } else if (address !== "") {
+    errorAddress.innerHTML = "";
+  }
+  if (levels.selectedIndex == -1) {
+    errorLevel.innerHTML = "هذه الخانة مطلوبة";
+    // console.log("Empty");
+    return false;
+  } else if (levels.selectedIndex !== -1) {
+    errorLevel.innerHTML = "";
+    // return true;
+  }
+  // if (!pattern.test(dateValue) || dateValue == "") {
+  //   errorDate.innerHTML = "هذه الخانة مطلوبة";
+  // console.log("Empty");
+  //   return false;
+  // } else if (dateValue !== "") {
+  //   errorDate.innerHTML = "";
+  // return true;
+}
+function InfoValidation() {
+  const errorCompany = document.getElementById("cError"),
+    errorUsername = document.getElementById("fillError"),
+    errorPhone = document.getElementById("phError"),
+    errorEmail = document.getElementById("eError");
+
+  const companyName = document.forms.RegForm.company.value,
+    username = document.forms.RegForm.fill.value,
+    phoneNum = document.forms.RegForm.phone.value,
+    userEmail = document.forms.RegForm.email.value;
+  const regPhone = /^(\+201|01|00201)[0-2,5]{1}[0-9]{8}/g; // Javascript reGex for Phone Number validation.
+  const mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; // Javascript reGex for email validation.
+
+  if (companyName == "") {
+    errorCompany.innerHTML = "هذه الخانة مطلوبة";
+    // console.log("Empty");
+    return false;
+  } else if (companyName !== "") {
+    errorCompany.innerHTML = "";
+    company.style.border = "1px solid #28A745";
+  }
+  if (username == "") {
+    errorUsername.innerHTML = "هذه الخانة مطلوبة";
+    // console.log("Empty");
+    return false;
+  } else if (username !== "") {
+    errorUsername.innerHTML = "";
+    username.style.border = "1px solid #28A745;";
+  }
+
+  if (phoneNum == "") {
+    errorPhone.innerHTML = "هذه الخانة مطلوبة";
+    return false;
+  } else if (!regPhone.test(phoneNum)) {
+    errorPhone.innerHTML = `برجاء ادخال رقم موبايل واتساب صحيح (بالأرقام الانجليزية) يعمل في مصر بدون كود الدولة مثال: 01012345678`;
+    return false;
+  } else if (phoneNum !== "" || regPhone.test(phoneNum)) {
+    errorPhone.innerHTML = "";
+  }
+
+  if (userEmail == "") {
+    errorEmail.innerHTML = "هذه الخانة مطلوبة";
+    // console.log("Empty");
+    return false;
+  } else if (!mailformat.test(userEmail)) {
+    errorEmail.innerHTML = `برجاء ادخال ايميل صحيح (بالانجليزية)  مثال: ahmed@gmail.com`;
+    return false;
+  } else if (userEmail !== "" || mailformat.test(userEmail)) {
+    errorEmail.innerHTML = "";
+  }
 }
